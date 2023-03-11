75c40576f2d554cdb3272a0a8d06306c3b92aaa9
Kevin Seon <apkpj@Kevins-MacBook-Pro.local>
2023-03-10 21:01:55 -0600

    quick change to update

--- pages/about.tsx
+++ pages/about.tsx
@@ -21,32 +21,23 @@ export const About = (): JSX.Element => {
           title: 'About',
         }}
       >
-        <h1>Hi!</h1>
-        <p>
-          I am Kevin (Minwoo) Seon, I am currently a Junior studying Computer
-          Science and Math at UT Austin.
+        <h1>Ab0ut</h1>
+        <p className="mr-12 ">
+          I am Minwoo (Kevin) Seon, I am currently a Junior studying Computer
+          Science at UT Austin.
         </p>
-        <p>
-          My interests are mostly in SWE and CyberSecurity, but I am still
-          exploring things! I love trying to break things (most of the time,
-          unintentionally breaking things...) to see how they work. I love
-          participating in CTFs or other cybersecurity related competitions ({' '}
-          <a href="https://www.nationalccdc.org/">NCCDC</a> 4th Place last
-          year!), and creating different type of projects in my free time. Other
-          things I love to do in my free time are playing the violin, enjoying
-          video games, going to the gym, and playing shogi (Japanese chess, I am
-          really rusty... so I am trying to get back into it).
+        <p className="mr-12 ">
+          My interests are mostly in SWE and CyberSecurity, and I also love
+          playing games. Whenever I feel like I&apos;m not productive enough,
+          I&apos;m usually particapating in random cs and math competitions or
+          CTFs.
         </p>
-        <p>
-          In the upcoming Fall semester, I will be working at Silicon Labs as a
-          Robotic Process Automation Intern. I am looking for opportunities for
-          Summer 2023 so feel free to contact me at{' '}
-          <a href="mailto: mwseon@gmail.com">mwseon@gmail.com</a> for any
-          opportunities, questions, or resume.
+        <p className="mr-12 ">
+          My email is mwseon@gmail.com and my discord is Apkpj#0930 if you want
+          to reach out!
         </p>
-        <p className="text-xs">
-          This website may or may not be running on vulnerable versions of
-          packages... but there&apos;s nothing worth your time I promise!
+        <p className="text-xs mr-12 ">
+          I love free food plz lmk if there are any free food anywhere
         </p>
       </Layout>
     </motion.div>
